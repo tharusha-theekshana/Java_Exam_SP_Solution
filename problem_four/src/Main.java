113715b98d9d5605feeb5b9e6b24cd8174c8664e
Theekshana <theenutheekshana@gmail.com>
2025-03-28 13:00:13 +0530

    Problem four rest code.

--- problem_four/src/Main.java
+++ problem_four/src/Main.java
@@ -1,11 +1,25 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        int formedNumber;
+
+        // Define list
+        Integer[] numbers = new Integer[]{50, 2, 1,9};
+
+        // Convert these arrays to lists
+        List<Integer> intList = Arrays.asList(numbers);
+
+        formedNumber = calculateFormedNumber(intList);
+        System.out.println("Formed Number is : " + formedNumber);
 
     }
 
     public static int calculateFormedNumber(List list){
 
+        Collections.sort(list);
+        return 10;
     }
 }
